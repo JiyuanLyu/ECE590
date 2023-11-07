3379cd86d79337c0a6c95493777f28c0f8a030ef
JiyuanLyu <chelsealyu0425@ucla.edu>
2023-11-06 19:41:09 -0500

    write test

--- hw5/rmdup.py
+++ hw5/rmdup.py
@@ -2,6 +2,12 @@
 # This function keeps the last occurence of each element
 # and preserves order.
 # So rmdup([1,2,3,2,1,4,2]) should return [3,1,4,2]
+
+# Import necessary package
+import time
+import random
+
+# write the function removes duplicates
 def rmdup(data):
     # Write me
     last_occur = []
@@ -16,9 +22,57 @@ def rmdup(data):
     last_occur.reverse()
     return last_occur
 
-data = [1,2,3,2,1,4,2]
-result = rmdup(data)
-print(result)
+# test the function
+#data = [1,2,3,2,1,4,2]
+#result = rmdup(data)
+#print(result)
+
+# write a function to generate different data size
+def getSize(size):
+    sizeArr = []
+    while size < 4194304:
+        sizeArr.append(size)
+        size *= 2
+    return sizeArr
+
+# write a function to get data
+def getData(size):
+    dataMany = []
+    dataModerate = []
+    dataRare = []
+
+    for i in range(size):
+        dataMany.append(random.randrange(0, size/2048))
+        dataModerate.append(random.randrange(0, size/16))
+        dataRare.append(random.randrange(0, size/4))
+    return [dataMany, dataModerate, dataRare]
+
+# write a function to generate data
+def getTime(sizeArr):
+    timeMany = []
+    timeModerate = []
+    timeRare = []
+    for i in range(len(sizeArr)):
+        dataMany = getData(sizeArr[i])[0]
+        timeMany.append(rmdup(dataMany))
+        dataModerate = getData(sizeArr[i])[1]
+        timeModerate.append(rmdup(dataModerate))
+        dataRare = getData(sizeArr[i])[2]
+        timeRare.append(rmdup(dataRare))
+
+    return [timeMany, timeModerate, timeRare]
+
+def main():
+    sizeArr = getSize(4096)
+    timeArr = getTime(sizeArr)
+    for i in range(len(sizeArr)):
+        for j in range(len(timeArr)):
+            print(sizeArr[i], ",",
+                  timeArr[0][j], ",",
+                  timeArr[1][j], ",",
+                  timeArr[2][j], "\n")
+    return 0
 
 
-        
+if __name__ == "__main__":
+    main()
---
+++ hw5/test.py
@@ -0,0 +1,2 @@
+import random
+print(random.randrange(0, 8192/2048))
